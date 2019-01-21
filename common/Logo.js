60645c63fc60b6c09918a42d81ab95caacf38950
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-21 09:33:59 -0500

    Logo component is working

--- common/Logo.js
+++ common/Logo.js
@@ -5,13 +5,11 @@ export default class Logo extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.logoContainer}>
-                    <Image
-                        style={styles.logo}
-                        source={require('../assets/icon.png')}
-                        resizeMode="contain"
-                    />
-                </View>
+                <Image
+                    style={styles.logo}
+                    source={require('./icon.png')}
+                    resizeMode="contain"
+                />
             </View>
         )
     }
@@ -23,15 +21,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    logoContainer: {
-        flex: 1,
-        marginTop: 30
-    },
     logo: {
         justifyContent: 'center',
         alignItems: 'center',
-        flex: 1,
-        height: undefined, 
-        width: undefined
     }
 });
