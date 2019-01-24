eba9859d8e60a990009a1801b61de26e588f2e04
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-24 12:03:56 -0500

    Paso 2: Importar las dependencias que necesitamos en index.js

--- common/Boton/styles.js
+++
@@ -1,4 +0,0 @@
-import React, { Component } from 'react'; 
-import { Text, TouchableOpacity, View, ViewPropTypes } from 'react-native'; 
-import Base, { Rojo, Gris} from './styles';
-import PropTypes from 'prop-types';
