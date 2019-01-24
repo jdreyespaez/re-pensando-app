724a0c5a22986b1944097e29d45282441c45118a
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-24 12:09:29 -0500

    Paso 3: Se definen el componente, los PropTypes que usará y los métodos que necesita (usarPlantilla() y render())

--- common/Boton/index.js
+++ common/Boton/index.js
@@ -2,3 +2,20 @@ import React, { Component } from 'react';
 import { Text, TouchableOpacity, View, ViewPropTypes } from 'react-native'; 
 import Base, { Rojo, Gris} from './styles';
 import PropTypes from 'prop-types';
+
+const { array, string, object, bool, func, any } = PropTypes;
+
+class Boton extends Component {
+    static propTypes = {
+        // Se definirán en el Paso 4.
+    }
+    getTheme() {
+        // Se definirá en el Paso 5.
+    }
+
+    render() {
+        // Se definirá en el Paso 6.
+    }
+};
+
+export default Boton;
