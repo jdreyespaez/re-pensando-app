8701f0fc259cca145b5589420987be11c9382f89
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-30 15:25:25 -0500

    Campos de texto para formulario Colegio agregados

--- common/Form.js
+++ common/Form.js
@@ -4,7 +4,7 @@ import {
     Text, 
     View,
     TextInput,
-    TouchableOpacity 
+    TouchableOpacity,
 } from 'react-native';
 
 export default class Regform extends React.Component {
@@ -12,6 +12,36 @@ export default class Regform extends React.Component {
         return (
             <View style={styles.regform}>
                 <Text style={styles.header}>Colegio</Text>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.inputTitle}>
+                        NOMBRE COLEGIO
+                    </Text>
+                    <TextInput style={styles.textInput}/>
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.inputTitle}>
+                        NOMBRES
+                    </Text>
+                    <TextInput style={styles.textInput}/>
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.inputTitle}>
+                        APELLIDOS
+                    </Text>
+                    <TextInput style={styles.textInput}/>
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.inputTitle}>
+                        CORREO
+                    </Text>
+                    <TextInput style={styles.textInput}/>
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.inputTitle}>
+                        BARRIO
+                    </Text>
+                    <TextInput style={styles.textInput}/>
+                </View>
             </View>
         );
     }
@@ -20,11 +50,33 @@ export default class Regform extends React.Component {
 const styles = StyleSheet.create({
     regform: {
         alignSelf: 'stretch',
+        marginLeft: 10,
+        marginRight: 10,
     },
     header: {
         fontSize: 30,
+        fontWeight: 'bold',
         color: '#e02c2c',
-        paddingBottom: 10,
+        paddingBottom: 20,
         textAlign: 'center',
+    },
+    inputContainer: {
+        alignSelf: 'stretch',
+        borderColor: '#979797',
+        borderWidth: 2,
+        height: 60,
+        marginBottom: 15,
+    },
+    inputTitle: {
+        color: '#979797',
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginLeft: 10,
+        marginTop: 5,
+    },
+    textInput: {
+        color: '#e74c3c',
+        height: 40,
+        marginLeft: 10,
     }
 });
