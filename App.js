4d5c2e8dbddd67c7262a31b4df67a0e4fd7e451b
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-03-06 16:56:40 -0500

    Incluyendo navegación para Home

--- App.js
+++ App.js
@@ -1,66 +1,66 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Image, Button } from 'react-native';
-import { Constants, Font } from 'expo';
-import { createStackNavigator, createAppContainer} from 'react-navigation';
-import RegisterScreen from './screens/RegisterScreen';
-import CompanyScreen from './screens/CompanyScreen';
-import SchoolScreen from './screens/SchoolScreen';
-import CongratsScreen from './screens/CongratsScreen';
-import HowToScreen from './screens/HowToScreen';
-import Logo from './common/Logo';
+import * as React from "react";
+import { Text, View, StyleSheet, Image, Button } from "react-native";
+import { Constants, Font } from "expo";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import RegisterScreen from "./screens/RegisterScreen";
+import CompanyScreen from "./screens/CompanyScreen";
+import SchoolScreen from "./screens/SchoolScreen";
+import CongratsScreen from "./screens/CongratsScreen";
+import HowToScreen from "./screens/HowToScreen";
+import Home from "./screens/Home";
+import Logo from "./common/Logo";
 
 class LoginScreen extends React.Component {
-
   static navigationOptions = {
-    header: null,
-  }
+    header: null
+  };
 
   onPress = () => {
-    this.props.navigation.push('RegisterScreen');
+    this.props.navigation.push("RegisterScreen");
   };
 
   render() {
-      return (
-          <View style={styles.container}>
-              <Logo />
-              <View style={styles.viewContainer}>
-                  <View style={styles.titleContainer}>
-                      <Text style={styles.title}>Bogotá sólo recicla el 15% de su basura.</Text>
-                  </View>
-                  <Text style={styles.subtitle}>¿Quieres ser parte de este proyecto colaborativo para REDUCIR LA BASURA EN BOGOTÁ?</Text>
-                  <View style={styles.buttonContainer}>
-                      <Button
-                          title="Regístrate"
-                          color="#D70026"
-                          onPress={this.onPress}
-                      />
-                  </View>
-              </View>
+    return (
+      <View style={styles.container}>
+        <Logo />
+        <View style={styles.viewContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>
+              Bogotá sólo recicla el 15% de su basura.
+            </Text>
           </View>
-      )
+          <Text style={styles.subtitle}>
+            ¿Quieres ser parte de este proyecto colaborativo para REDUCIR LA
+            BASURA EN BOGOTÁ?
+          </Text>
+          <View style={styles.buttonContainer}>
+            <Button title="Regístrate" color="#D70026" onPress={this.onPress} />
+          </View>
+        </View>
+      </View>
+    );
   }
 }
 
-const MainStackNavigator = createStackNavigator(
-  {
-    LoginScreen,
-    RegisterScreen,
-    SchoolScreen: {
-      screen: SchoolScreen,
-      navigationOptions: ({ navigation }) => ({
-        title: 'Perfil para ' + navigation.getParam('name'),
-      }),
-    },
-    CompanyScreen: {
-      screen: CompanyScreen,
-      navigationOptions: ({ navigation }) => ({
-        title: 'Perfil para ' + navigation.getParam('name'),
-      }),
-    },
-    CongratsScreen,
-    HowToScreen,
+const MainStackNavigator = createStackNavigator({
+  LoginScreen,
+  RegisterScreen,
+  SchoolScreen: {
+    screen: SchoolScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: "Perfil para " + navigation.getParam("name")
+    })
   },
-);
+  CompanyScreen: {
+    screen: CompanyScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: "Perfil para " + navigation.getParam("name")
+    })
+  },
+  CongratsScreen,
+  HowToScreen,
+  Home
+});
 
 // Link the back button on Android and manage the environment.
 
@@ -70,49 +70,47 @@ const NavigationApp = createAppContainer(MainStackNavigator);
 
 export default NavigationApp;
 
-
 /* CRAZY V3 PRO TIP: Native Navigation Containers */
-import { useScreens } from 'react-native-screens';
+import { useScreens } from "react-native-screens";
 useScreens();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    padding: 8,
+    padding: 8
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center"
   },
-title: {
+  title: {
     fontSize: 25,
-    fontWeight: 'bold',
-    color: 'black',
+    fontWeight: "bold",
+    color: "black",
     // fontFamily: 'lato-black',
-    textAlign: 'center'
-},
-titleContainer: {
+    textAlign: "center"
+  },
+  titleContainer: {
     marginBottom: 25
-},
-subtitle: {
+  },
+  subtitle: {
     fontSize: 20,
-    color: 'black',
+    color: "black",
     // fontFamily: 'lato-regular',
-    textAlign: 'center'
-},
-viewContainer: {
+    textAlign: "center"
+  },
+  viewContainer: {
     flex: 1,
-    backgroundColor:'white',
+    backgroundColor: "white",
     margin: 40
-},
-buttonContainer: {
+  },
+  buttonContainer: {
     margin: 40,
-    fontSize: 80,
+    fontSize: 80
     // fontFamily: 'lato-regular'
-}
+  }
 });
-
